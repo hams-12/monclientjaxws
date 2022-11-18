ebb4a758f97a7afe02d98188717bf57caf28ed60
Cheickna Hamala SANTARA <santarahamala@gmail.com>
2022-11-18 16:01:46 +0100

    Après generation à partir du wsdl et ecriture de la classe ClientWS

---
+++ src/main/java/proxy/ObjectFactory.java
@@ -0,0 +1,189 @@
+
+package proxy;
+
+import javax.xml.namespace.QName;
+import jakarta.xml.bind.JAXBElement;
+import jakarta.xml.bind.annotation.XmlElementDecl;
+import jakarta.xml.bind.annotation.XmlRegistry;
+
+
+/**
+ * This object contains factory methods for each 
+ * Java content interface and Java element interface 
+ * generated in the proxy package. 
+ * <p>An ObjectFactory allows you to programatically 
+ * construct new instances of the Java representation 
+ * for XML content. The Java representation of XML 
+ * content can consist of schema derived interfaces 
+ * and classes representing the binding of schema 
+ * type definitions, element declarations and model 
+ * groups.  Factory methods for each of these are 
+ * provided in this class.
+ * 
+ */
+@XmlRegistry
+public class ObjectFactory {
+
+    private final static QName _Convert_QNAME = new QName("http://ws/", "Convert");
+    private final static QName _ConvertResponse_QNAME = new QName("http://ws/", "ConvertResponse");
+    private final static QName _GetCompte_QNAME = new QName("http://ws/", "getCompte");
+    private final static QName _GetCompteResponse_QNAME = new QName("http://ws/", "getCompteResponse");
+    private final static QName _Listecompte_QNAME = new QName("http://ws/", "listecompte");
+    private final static QName _ListecompteResponse_QNAME = new QName("http://ws/", "listecompteResponse");
+
+    /**
+     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: proxy
+     * 
+     */
+    public ObjectFactory() {
+    }
+
+    /**
+     * Create an instance of {@link Convert }
+     * 
+     * @return
+     *     the new instance of {@link Convert }
+     */
+    public Convert createConvert() {
+        return new Convert();
+    }
+
+    /**
+     * Create an instance of {@link ConvertResponse }
+     * 
+     * @return
+     *     the new instance of {@link ConvertResponse }
+     */
+    public ConvertResponse createConvertResponse() {
+        return new ConvertResponse();
+    }
+
+    /**
+     * Create an instance of {@link GetCompte }
+     * 
+     * @return
+     *     the new instance of {@link GetCompte }
+     */
+    public GetCompte createGetCompte() {
+        return new GetCompte();
+    }
+
+    /**
+     * Create an instance of {@link GetCompteResponse }
+     * 
+     * @return
+     *     the new instance of {@link GetCompteResponse }
+     */
+    public GetCompteResponse createGetCompteResponse() {
+        return new GetCompteResponse();
+    }
+
+    /**
+     * Create an instance of {@link Listecompte }
+     * 
+     * @return
+     *     the new instance of {@link Listecompte }
+     */
+    public Listecompte createListecompte() {
+        return new Listecompte();
+    }
+
+    /**
+     * Create an instance of {@link ListecompteResponse }
+     * 
+     * @return
+     *     the new instance of {@link ListecompteResponse }
+     */
+    public ListecompteResponse createListecompteResponse() {
+        return new ListecompteResponse();
+    }
+
+    /**
+     * Create an instance of {@link Compte }
+     * 
+     * @return
+     *     the new instance of {@link Compte }
+     */
+    public Compte createCompte() {
+        return new Compte();
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link Convert }{@code >}
+     * 
+     * @param value
+     *     Java instance representing xml element's value.
+     * @return
+     *     the new instance of {@link JAXBElement }{@code <}{@link Convert }{@code >}
+     */
+    @XmlElementDecl(namespace = "http://ws/", name = "Convert")
+    public JAXBElement<Convert> createConvert(Convert value) {
+        return new JAXBElement<>(_Convert_QNAME, Convert.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link ConvertResponse }{@code >}
+     * 
+     * @param value
+     *     Java instance representing xml element's value.
+     * @return
+     *     the new instance of {@link JAXBElement }{@code <}{@link ConvertResponse }{@code >}
+     */
+    @XmlElementDecl(namespace = "http://ws/", name = "ConvertResponse")
+    public JAXBElement<ConvertResponse> createConvertResponse(ConvertResponse value) {
+        return new JAXBElement<>(_ConvertResponse_QNAME, ConvertResponse.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link GetCompte }{@code >}
+     * 
+     * @param value
+     *     Java instance representing xml element's value.
+     * @return
+     *     the new instance of {@link JAXBElement }{@code <}{@link GetCompte }{@code >}
+     */
+    @XmlElementDecl(namespace = "http://ws/", name = "getCompte")
+    public JAXBElement<GetCompte> createGetCompte(GetCompte value) {
+        return new JAXBElement<>(_GetCompte_QNAME, GetCompte.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link GetCompteResponse }{@code >}
+     * 
+     * @param value
+     *     Java instance representing xml element's value.
+     * @return
+     *     the new instance of {@link JAXBElement }{@code <}{@link GetCompteResponse }{@code >}
+     */
+    @XmlElementDecl(namespace = "http://ws/", name = "getCompteResponse")
+    public JAXBElement<GetCompteResponse> createGetCompteResponse(GetCompteResponse value) {
+        return new JAXBElement<>(_GetCompteResponse_QNAME, GetCompteResponse.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link Listecompte }{@code >}
+     * 
+     * @param value
+     *     Java instance representing xml element's value.
+     * @return
+     *     the new instance of {@link JAXBElement }{@code <}{@link Listecompte }{@code >}
+     */
+    @XmlElementDecl(namespace = "http://ws/", name = "listecompte")
+    public JAXBElement<Listecompte> createListecompte(Listecompte value) {
+        return new JAXBElement<>(_Listecompte_QNAME, Listecompte.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link ListecompteResponse }{@code >}
+     * 
+     * @param value
+     *     Java instance representing xml element's value.
+     * @return
+     *     the new instance of {@link JAXBElement }{@code <}{@link ListecompteResponse }{@code >}
+     */
+    @XmlElementDecl(namespace = "http://ws/", name = "listecompteResponse")
+    public JAXBElement<ListecompteResponse> createListecompteResponse(ListecompteResponse value) {
+        return new JAXBElement<>(_ListecompteResponse_QNAME, ListecompteResponse.class, null, value);
+    }
+
+}
